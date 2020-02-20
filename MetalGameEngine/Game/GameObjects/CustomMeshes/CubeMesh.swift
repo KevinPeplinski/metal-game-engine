d51f2c91cc6ffdfa76268a2bde6fe23b4407e67f
Kevin Peplinski <k.peplinski@gmx.de>
2020-02-20 20:47:33 +0100

    update cube normals

--- MetalGameEngine/Game/GameObjects/CustomMeshes/CubeMesh.swift
+++ MetalGameEngine/Game/GameObjects/CustomMeshes/CubeMesh.swift
@@ -11,52 +11,52 @@ class CubeMesh: CustomMesh {
     override func createVertices() {
         self.vertices = [
              //Left
-             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0,-1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
+             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(-1,0,0)),
+             Vertex(position: SIMD3<Float>(-1.0,-1.0, 1.0), normal: SIMD3<Float>(-1,0,0)),
+             Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), normal: SIMD3<Float>(-1,0,0)),
+             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(-1,0,0)),
+             Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), normal: SIMD3<Float>(-1,0,0)),
+             Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), normal: SIMD3<Float>(-1,0,0)),
              
              //RIGHT
-             Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
+             Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,0)),
+             Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,0)),
+             Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), normal: SIMD3<Float>(1,0,0)),
+             Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,0)),
+             Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,0)),
+             Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), normal: SIMD3<Float>(1,0,0)),
              
              //TOP
-             Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
+             Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), normal: SIMD3<Float>(0,1,0)),
+             Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), normal: SIMD3<Float>(0,1,0)),
+             Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), normal: SIMD3<Float>(0,1,0)),
+             Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), normal: SIMD3<Float>(0,1,0)),
+             Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), normal: SIMD3<Float>(0,1,0)),
+             Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), normal: SIMD3<Float>(0,1,0)),
              
              //BOTTOM
-             Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0,-1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
+             Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), normal: SIMD3<Float>(0,-1,0)),
+             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(0,-1,0)),
+             Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), normal: SIMD3<Float>(0,-1,0)),
+             Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), normal: SIMD3<Float>(0,-1,0)),
+             Vertex(position: SIMD3<Float>(-1.0,-1.0, 1.0), normal: SIMD3<Float>(0,-1,0)),
+             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(0,-1,0)),
              
              //BACK
-             Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(1,0,1)),
+             Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), normal: SIMD3<Float>(0,0,-1)),
+             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(0,0,-1)),
+             Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), normal: SIMD3<Float>(0,0,-1)),
+             Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), normal: SIMD3<Float>(0,0,-1)),
+             Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), normal: SIMD3<Float>(0,0,-1)),
+             Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), normal: SIMD3<Float>(0,0,-1)),
              
              //FRONT
-             Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0,-1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
-             Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), normal: SIMD3<Float>(1,0,1)),
+             Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), normal: SIMD3<Float>(0,0,1)),
+             Vertex(position: SIMD3<Float>(-1.0,-1.0, 1.0), normal: SIMD3<Float>(0,0,1)),
+             Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), normal: SIMD3<Float>(0,0,1)),
+             Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), normal: SIMD3<Float>(0,0,1)),
+             Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), normal: SIMD3<Float>(0,0,1)),
+             Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), normal: SIMD3<Float>(0,0,1)),
          ]
     }
     
