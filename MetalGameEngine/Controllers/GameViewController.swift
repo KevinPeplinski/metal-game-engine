021da0efe04d2d36395fce5f2c45dbd88f0dba09
Kevin Peplinski <k.peplinski@gmx.de>
2020-03-01 19:33:23 +0100

    add city Label

--- MetalGameEngine/Controllers/GameViewController.swift
+++ MetalGameEngine/Controllers/GameViewController.swift
@@ -14,8 +14,12 @@ class GameViewController: UIViewController {
     private var renderer: Renderer!
     private var game: Game = Game()
     
+    // .start
     @IBOutlet weak var startButton: UIButton!
+    
+    // .inRound
     @IBOutlet weak var locateButton: UIButton!
+    @IBOutlet weak var cityLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,39 +36,52 @@ class GameViewController: UIViewController {
         
         Engine.ignite(device: metalView.device!)
         
-        self.locateButton.isHidden = true
+        self.hideAllUIElementsNotInStart()
     }
     
-    @IBAction func handlePan(_ gesture: UIPanGestureRecognizer) {
-        guard let gestureView = gesture.view else {
-            return
+    private func hideAllUIElementsNotInStart() -> Void {
+        self.locateButton.isHidden = true
+        self.cityLabel.isHidden = true
+    }
+    
+    // .start
+    // startButton Action
+    @IBAction func startGame(_ sender: Any) {
+        if StateManager.getState() == .start {
+            self.game.startRound()
+            self.locateButton.isHidden = false
+            self.startButton.isHidden = true
         }
-        switch gesture.state {
-        case .began:
-            EarthRotate.previousPanPoint = gesture.location(in: gestureView)
-        case .changed:
-            EarthRotate.rotate(gesture.location(in: gestureView))
-        default:
-            EarthRotate.previousPanPoint = nil
+    }
+    
+    // .inRound
+    @IBAction func handlePan(_ gesture: UIPanGestureRecognizer) {
+        if StateManager.getState() == .inRound {
+            guard let gestureView = gesture.view else {
+                return
+            }
+            switch gesture.state {
+            case .began:
+                EarthRotate.previousPanPoint = gesture.location(in: gestureView)
+            case .changed:
+                EarthRotate.rotate(gesture.location(in: gestureView))
+            default:
+                EarthRotate.previousPanPoint = nil
+            }
         }
     }
     
     @IBAction func handlePinch(_ gesture: UIPinchGestureRecognizer) {
-        EarthScale.scale(Float(gesture.scale))
+        if StateManager.getState() == .inRound {
+            EarthScale.scale(Float(gesture.scale))
+        }
     }
     
     // LocateButton Action
     @IBAction func getRotation(_ sender: Any) {
-
-        print(EarthRotate.getLatitudinal())
-        
-    }
-    
-    // startButton Action
-    @IBAction func startGame(_ sender: Any) {
-        self.game.startRound()
-        self.locateButton.isHidden = false
-        self.startButton.isHidden = true
+        if StateManager.getState() == .inRound {
+            print(EarthRotate.getLatitudinal())
+        }
     }
 }
 
